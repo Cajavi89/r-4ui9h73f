493d8789a6fdd503fe1b3a93669f3113a5568700
Carlos <cajavi89@gmail.com>
2021-11-29 22:03:21 -0500

    Tercero reto, repetidor de palabras

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      repeater: ''
+    }
+    this.theRepeater= this.theRepeater.bind(this)
+  }
+
+  theRepeater(e){
+    this.setState({
+     repeater: e.target.value
+    })
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input onChange={this.theRepeater} type="text" placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.repeater}</p>
       </div>
     );
   }
